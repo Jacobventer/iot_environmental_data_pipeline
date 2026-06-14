11033cae0ccb6b728b912451a7738c7e0fc1c0a6
Jaco Venter <149387555+Jacobventer@users.noreply.github.com>
2026-06-14 17:11:43 +0200

    Update init_db.py

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -9,14 +9,14 @@ the environmental pipeline.
 from pymongo import MongoClient
 import os
 
-# Connect to MongoDB 
+#Connect to MongoDB 
 mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 client = MongoClient(mongo_uri)
 
-# Create database
+#Create database
 db = client["iot_environment"]
 
-# Create collection
+#Create collection
 collection = db["sensor_readings"]
 
 collection.create_index("device")
